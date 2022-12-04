58aff7ac5cde0adf48ce103b0ed046be7c283dde
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2022-12-04 14:42:33 +0800

    Initial commit

---
+++ config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :ledger, Ledger.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "ledger_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :ledger, LedgerWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "Ws3HcpF2lSfZEzt0qHG7r0mDoRZJCmNHk73RAImUqURF064GshgAAlrQFexRer3C",
+  server: false
+
+# In test we don't send emails.
+config :ledger, Ledger.Mailer, adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
