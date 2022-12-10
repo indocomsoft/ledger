32baa6d7f83b76269b39c399532222b6afb26861
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2022-12-10 16:40:01 +0800

    Add tests for AuthController

---
+++ test/ledger_web/controllers/auth_controller_test.exs
@@ -0,0 +1,47 @@
+defmodule LedgerWeb.AuthControllerTest do
+  use LedgerWeb.ConnCase, async: true
+  import Ledger.AccountsFixtures
+
+  describe "create/2" do
+    setup do
+      conn = build_conn() |> put_req_header("content-type", "application/json")
+      %{user: user_fixture(), conn: conn}
+    end
+
+    @spec login(Plug.Conn.t(), String.t(), String.t()) :: Plug.Conn.t()
+    defp login(conn, username, password) do
+      post(
+        conn,
+        Routes.auth_path(conn, :create),
+        Jason.encode!(%{"username" => username, "password" => password})
+      )
+    end
+
+    test "returns session token for correct username and password", %{
+      user: %{id: user_id, username: username},
+      conn: conn
+    } do
+      assert %{"token" => token} =
+               login(conn, username, valid_user_password()) |> json_response(200)
+
+      {:ok, verified_token} = LedgerWeb.Auth.verify(token)
+      assert %{id: ^user_id} = Ledger.Accounts.get_user_by_session_token(verified_token)
+    end
+
+    test "returns 401 on wrong username/password", %{user: %{username: username}, conn: conn} do
+      assert login(conn, "invalid", valid_user_password()) |> json_response(401)
+
+      assert login(conn, username, "invalid") |> json_response(401)
+    end
+
+    test "returns 400 on malformed request", %{conn: conn} do
+      conn =
+        post(conn, Routes.auth_path(conn, :create), %{
+          "email" => "hello@example.com",
+          "password" => "never gonna give you up"
+        })
+
+      assert json_response(conn, 400)
+    end
+  end
+end
