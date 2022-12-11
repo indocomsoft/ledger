3cb6fed2476b9e65c151f962e17172c47469657a
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2022-12-11 20:03:28 +0800

    Make auth tokens not expire on the phoenix token layer

--- lib/ledger_web/auth.ex
+++ lib/ledger_web/auth.ex
@@ -1,17 +1,19 @@
 defmodule LedgerWeb.Auth do
   @moduledoc """
   Provides helpers for signing user session tokens and verifying them.
+
+  All tokens are marked as not expiring because expiry is handled through dates in the DB.
   """
 
   @token_signing_salt "user auth"
 
   @spec sign(binary()) :: String.t()
   def sign(token) when is_binary(token) do
-    Phoenix.Token.sign(LedgerWeb.Endpoint, @token_signing_salt, token)
+    Phoenix.Token.sign(LedgerWeb.Endpoint, @token_signing_salt, token, max_age: :infinity)
   end
 
   @spec verify(String.t()) :: {:ok, binary()} | {:error, atom()}
   def verify(token) when is_binary(token) do
-    Phoenix.Token.verify(LedgerWeb.Endpoint, @token_signing_salt, token)
+    Phoenix.Token.verify(LedgerWeb.Endpoint, @token_signing_salt, token, max_age: :infinity)
   end
 end
