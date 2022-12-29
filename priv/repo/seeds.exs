5e84d49d89fc7871c8c069ca3318b00385eba51f
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2022-12-29 14:55:59 +0700

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,72 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+{:ok, user} = Ledger.Users.register_user("test", "test12345678", "SGD")
+Ledger.Repo.put_user_id(user.id)
+
+root_account = Ledger.Book.create_or_get_root_account_for_user!(user)
+
+{:ok, income} =
+  Ledger.Book.create_child_account(root_account, %{
+    "account_type" => "income",
+    "currency" => "SGD",
+    "name" => "Income",
+    "placeholder" => true
+  })
+
+{:ok, salary} =
+  Ledger.Book.create_child_account(income, %{
+    "account_type" => "income",
+    "currency" => "SGD",
+    "name" => "Salary",
+    "placeholder" => false
+  })
+
+{:ok, bonus} =
+  Ledger.Book.create_child_account(income, %{
+    "account_type" => "income",
+    "currency" => "SGD",
+    "name" => "Bonus",
+    "placeholder" => false
+  })
+
+{:ok, expense} =
+  Ledger.Book.create_child_account(root_account, %{
+    "account_type" => "expense",
+    "currency" => "SGD",
+    "name" => "Expense",
+    "placeholder" => true
+  })
+
+{:ok, tax} =
+  Ledger.Book.create_child_account(root_account, %{
+    "account_type" => "expense",
+    "currency" => "SGD",
+    "name" => "Tax",
+    "placeholder" => true
+  })
+
+{:ok, us_federal_tax} =
+  Ledger.Book.create_child_account(root_account, %{
+    "account_type" => "expense",
+    "currency" => "USD",
+    "name" => "US Federal Tax",
+    "placeholder" => false
+  })
+
+{:ok, us_state_tax} =
+  Ledger.Book.create_child_account(root_account, %{
+    "account_type" => "expense",
+    "currency" => "USD",
+    "name" => "US State Tax",
+    "placeholder" => false
+  })
+
+{:ok, sg_tax} =
+  Ledger.Book.create_child_account(root_account, %{
+    "account_type" => "expense",
+    "currency" => "SGD",
+    "name" => "SG Tax",
+    "placeholder" => false
+  })
